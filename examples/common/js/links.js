294780757978b25c78909f3367ff130cac764a07
Jonathan Turner <jonathan.a.turner@comcast.net>
2023-11-28 17:18:01 -0500

    renaming

--- examples/common/js/links.js
+++ examples/common/js/links.js
@@ -1,6 +1,6 @@
 import {a} from "../../src/jtml.js";
 
-export default let links = [
+export default links = [
     a("../bindings-1", "Basic Bindings"),
     a("../lists", "Lists"),
     a("../select", "Select")
